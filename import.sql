fe4c08113cb06fd96d13d3e923e761ef1612a9c1
Moss <2103702@talnet.nl>
2022-11-22 16:42:25 +0100

    database structure

--- import.sql
+++ import.sql
@@ -1,24 +1,34 @@
+drop database if exists vaversa;
 CREATE DATABASE vaversa;
 
-USE DATABASE vaversa;
+use vaversa;
 
 CREATE TABLE users (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username varchar(20) NOT NULL,
-    password varchar(30) NOT NULL
+	Id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	username varchar(20) NOT NULL,
+	password varchar(30) NOT NULL
 );
 
-CREATE TABLE plants (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name varchar(30) NOT NULL,
-    description text,
-    identifier int
+CREATE TABLE plantInfo (
+	Id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name varchar(30) NOT NULL,
+	attributes text,
+	water DATE NOT NULL,
+	time_until_harvist DATE,
+	light varchar(30), 
+	trims varchar(30)
 );
 
-CREATE TABLE refills (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    plants_id int NOT NULL,
-    title varchar(20) NOT NULL,
-    start_time DATE NOT NULL,
-    end_time DATE NOT NULL
+CREATE TABLE plant (
+	Id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	plant_date DATE,
+	last_water DATE,
+	kasId int,
+	Foreign KEY (kasId) references kas(Id),
+);
+
+CREATE TABLE kas (
+	Id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	userId int,
+	Foreign KEY (userId) references users(Id)
 );
